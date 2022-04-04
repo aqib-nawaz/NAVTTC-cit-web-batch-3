ab8e84f4a67f68c0c18c03749186bed83e65954d
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-04 14:48:54 +0500

    If else conditional statement

---
+++ week-4/javascript/playground/basics.js
@@ -0,0 +1,21 @@
+// If els condition
+const marks = 20;
+
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A");
+} else if (marks >= 60) {
+  console.log("A-");
+} else if (marks >= 50) {
+  // else if is optional
+  console.log("B");
+} else if (marks >= 40) {
+  console.log("C");
+} else if (marks >= 30) {
+  console.log("D");
+} else {
+ console.log("F");
+}
+
+//
