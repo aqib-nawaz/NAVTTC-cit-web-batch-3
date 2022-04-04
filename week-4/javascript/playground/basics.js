13e4418bfc6087365ae916b61afe057fc3662cca
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-04 16:28:42 +0500

    object manipulation

--- week-4/javascript/playground/basics.js
+++ week-4/javascript/playground/basics.js
@@ -1,42 +1,119 @@
 // If els condition
-const marks = 20;
+// const marks = 20;
 
-if (marks >= 80) {
-  console.log("A+");
-} else if (marks >= 70) {
-  console.log("A");
-} else if (marks >= 60) {
-  console.log("A-");
-} else if (marks >= 50) {
-  // else if is optional
-  console.log("B");
-} else if (marks >= 40) {
-  console.log("C");
-} else if (marks >= 30) {
-  console.log("D");
-} else {
- console.log("F");
+// if (marks >= 80) {
+//   console.log("A+");
+// } else if (marks >= 70) {
+//   console.log("A");
+// } else if (marks >= 60) {
+//   console.log("A-");
+// } else if (marks >= 50) {
+//   // else if is optional
+//   console.log("B");
+// } else if (marks >= 40) {
+//   console.log("C");
+// } else if (marks >= 30) {
+//   console.log("D");
+// } else {
+//  console.log("F");
+// }
+
+// //Template literals
+
+// (marks > 30) ? console.log("Pass") : console.log("You are Fail");
+
+// // switch case  
+// switch (marks) {
+//     case 10:
+//         console.log("Equal to 10")
+//         break;
+//     case 20:
+//         console.log("Equal to 20")
+//         break;
+//     case 30:
+//         console.log("Equal to 30")
+//         break;
+
+//     default:
+//         console.log("Not equal to 10, 20, 30");
+// }
+
+
+
+var data = {
+  marks: 45,
+  name : "Abdus Samad",
+  gpa: 3.7,
+  isPass: true,
+  friends : ["Aqib", "Hayat"],
+  address:{
+    city: "Mardan",
+    countary: "Pakistan"
+  }
+
 }
 
-//Template literals
+// Challenges 1
 
-(marks > 30) ? console.log("Pass") : console.log("You are Fail");
+// if isPass is true display all friends
+if(data.isPass){
+  console.log(data.friends)
+}
 
-// switch case  
-switch (marks) {
-    case 10:
-        console.log("Equal to 10")
-        break;
-    case 20:
-        console.log("Equal to 20")
-        break;
-    case 30:
-        console.log("Equal to 30")
-        break;
-
-    default:
-        console.log("Not equal to 10, 20, 30");
+// Challenges 2
+// if isPass is true, gpa > 3.5, and city  = Mardan display "You are eligible for scholarship"
+if((data.isPass) && (data.gpa > 3.5) && (data.address.city == "Mardan") ){
+  console.log("YOu are eligible for scholarship");
 }
 
 
 
+
+
+var studData = [
+  {
+    marks: 45,
+    name : "Abdus Samad",
+    gpa: 3.7,
+    isPass: true,
+    friends : ["Aqib", "Hayat"],
+    address:{
+      city: "DIK",
+      countary: "Pakistan"
+    }
+  },
+  {
+      marks: 45,
+      name : "Abdus Samad",
+      gpa: 3.7,
+      isPass: false,
+      friends : ["Aqib", "Hayat"],
+      address:{
+        city: "Mardan",
+        countary: "Pakistan"
+      }
+    
+  },
+  {
+    marks: 45,
+    name : "Abdus Samad",
+    gpa: 3.7,
+    isPass: true,
+    friends : ["Aqib", "Hayat"],
+    address:{
+      city: "Mardan",
+      countary: "Pakistan"
+    }
+  
+  }
+
+]
+// // Challenge 3
+
+// Display  cities of all the students who are passed
+for (let i = 0; i< studData.length; i++){
+
+  if(studData[i].isPass){
+    console.log(studData[i].address.city)
+  }
+}
