61cec3e5e024a68f6416fca8d1fab0d33f1e8940
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-05 16:19:44 +0500

    Data Serialization(csv, yaml, xml, json) - About JSON, challanges

---
+++ week-5-javascript/JSON/data.js
@@ -0,0 +1,45 @@
+var data = {
+    products: [
+      {
+        name: "Laptop",
+        price: 1000,
+        isAvailable: true,
+      },
+      {
+        name: "Mobile",
+        price: 500,
+        isAvailable: false,
+      },
+      {
+        name: "TV",
+        price: 2000,
+        isAvailable: false,
+      },
+      {
+        name: "Fridge",
+        price: 3000,
+        isAvailable: true,
+      },
+    ]
+  };
+  
+
+
+// Challenge--1  
+// Calculate Total Price
+
+let totalPrice = 0;
+let discountPrice;
+for(let i = 0; i < data.products.length; i++){
+    totalPrice += data.products[i].price
+
+    if(data.products[i].isAvailable){
+        
+        console.log(data.products[i].name+ " " + "Price : " + data.products[i].price)
+
+        data.products[i].price -= (data.products[i].price*20) /100;
+
+        console.log(data.products[i].name+" Discount Price : " + data.products[i].price)
+    }
+
+}
