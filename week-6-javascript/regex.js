8881eb615607caa3986beb462d3bd01d9a69a652
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-12 15:18:08 +0500

    regular expression

---
+++ week-6-javascript/regex.js
@@ -0,0 +1,9 @@
+const str = "Abdus Samad";
+
+const t = str.search(/Abdus Samad/i)
+const m = str.replace(/Abdus Samad/gi, "Abdus Khan")
+
+console.log(m)
+console.log(t);
+
+// website for practice regex : https://regex101.com/ | https://regexr.com/
